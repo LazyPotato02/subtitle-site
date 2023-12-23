500241f01d1bd740906a39b3acd6f2af3da2d692
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com>
2023-12-23 14:52:17 +0200

    Added put and delete  method to subtitle/api

--- back_end/subtitles/urls.py
+++ back_end/subtitles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import SubtitlesApiView
+from .views import SubtitlesApiView,SubtitleDetailedView
 
 urlpatterns = [
-    path('api/', SubtitlesApiView.as_view())
+    path('api/', SubtitlesApiView.as_view()),
+    path('api/<int:subtitle_id>',SubtitleDetailedView.as_view())
 ]
--- back_end/subtitles/views.py
+++ back_end/subtitles/views.py
@@ -10,12 +10,6 @@ from .serializers import SubtitlesSerializer
 class SubtitlesApiView(APIView):
     # permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self, todo_id):
-        try:
-            return Subtitles.objects.get(id=todo_id)
-        except Subtitles.DoesNotExist:
-            return None
-
     def get(self, request, *args, **kwargs):
         subtitles = Subtitles.objects.all()
         serializer = SubtitlesSerializer(subtitles, many=True)
@@ -34,3 +28,54 @@ class SubtitlesApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+class SubtitleDetailedView(APIView):
+    def get_object(self, subtitle_id):
+        try:
+            return Subtitles.objects.get(id=subtitle_id)
+        except Subtitles.DoesNotExist:
+            return None
+
+    def get(self, request, subtitle_id, *args, **kwargs):
+        subtitle_instance = self.get_object(subtitle_id)
+        if not subtitle_instance:
+            return Response(
+                {"res": "Object with subtitle_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = SubtitlesSerializer(subtitle_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, subtitle_id, *args, **kwargs):
+        subtitle_instance = self.get_object(subtitle_id)
+        if not subtitle_instance:
+            return Response(
+                {'res': 'Object with subtitle_id does not exists'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        data = {
+            'name': request.data.get('name'),
+            'type': request.data.get('type'),
+            'genre': request.data.get('genre')
+        }
+        serializer = SubtitlesSerializer(instance=subtitle_instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, subtitle_id, *args, **kwargs):
+        subtitle_instance = self.get_object(subtitle_id)
+        if not subtitle_instance:
+            return Response(
+                {'res': 'Object with subtitle_id does not exists'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        subtitle_instance.delete()
+        return Response(
+            {'res':'Object deleted!'},
+            status=status.HTTP_200_OK
+        )
