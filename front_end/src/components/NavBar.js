601695ea5dafe9da93f385334ad8cc9bad8272d6
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com.>
2024-01-03 23:03:35 +0200

    Added routes to react pages

---
+++ front_end/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <h1>AllTheSubs</h1>
+        </div>
+    );
+}
+export default NavBar;
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter,Routes, Route,} from 'react-router-dom';
+import Home from './components/Home'
+import NavBar from './components/NavBar'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <NavBar/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
