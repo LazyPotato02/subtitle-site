f041c35ae691c26a27e9eb62a3982ce934bfad5e
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com.>
2024-01-04 00:20:50 +0200

    Added style and view to react home page

--- front_end/src/components/Home.js
+++ front_end/src/components/Home.js
@@ -1,9 +1,83 @@
 import React from 'react';
 import './Home.css';
+import MoviePic from '../img/wl-op-16se.jpg'
 function Home() {
     return (
         <div className="mainPage">
-            
+            <div className={'movies'}>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+                <div className={'movie'}>
+                    <img src={MoviePic} alt="img"/>
+                    <p>Name: Test</p>
+                    <p>Genre: Test</p>
+                    <p>Year: Test</p>
+                    <p>Rating: Test</p>
+                </div>
+
+            </div>
+
         </div>
     );
 }
--- front_end/src/components/NavBar.js
+++ front_end/src/components/NavBar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import './NavBar.css'
 
 function NavBar() {
     return (
         <div className="NavBar">
-            <h1>AllTheSubs</h1>
+            <nav>
+                <h1><a className={'logo'} href="/">AllTheSubs</a></h1>
+                <div className={"links"}>
+                    <a href="">Movies</a>
+                    <a href="">Series</a>
+                    <a href="">Latest</a>
+                    <a href=""><i className="fas fa-search"></i></a>
+
+                </div>
+            </nav>
         </div>
     );
 }
+
 export default NavBar;
