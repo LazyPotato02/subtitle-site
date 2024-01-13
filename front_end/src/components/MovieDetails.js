510497e5c53f251311ab1848d25d563716225768
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-01-14 00:49:02 +0200

    Added style and img render

--- front_end/src/components/MovieDetails.js
+++ front_end/src/components/MovieDetails.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useParams} from 'react-router-dom';
-
+import './MovieDetails.css'
+import MoviePic from '../img/wl-op-16se.jpg'
 function MovieDetails (){
     const { id } = useParams();
     console.log(`Movie id is ${id}`)
     return (
-        <div>
-            <article>
-                
+        <div className={'movieMain'}>
+            <article className={'movieBody'}>
+                <img className={'img'} src={MoviePic} alt=""/>
             </article>
         </div>
     );
