4387ec098b419f0b4c08ab6250a4c8e51cc5b8dc
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-02-29 19:16:53 +0200

    Added models to admin panel

--- back_end/subtitles/admin.py
+++ back_end/subtitles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from subtitles.models import Subtitles
+
+
 # Register your models here.
+
+
+class SubtitlesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Subtitles, SubtitlesAdmin)
